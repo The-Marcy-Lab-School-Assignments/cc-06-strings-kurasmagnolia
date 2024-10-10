2820e90d73a7707eba0a545be5fdaf0e4997423a
kurasmagnolia <kingmmcleod@outlook.com>
2024-10-10 12:17:11 -0400

    finished all questions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 //write your code here
+// Question 1
+const reverseString = (string) => {
+  let finalStr = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    finalStr += string[i];
+  }
+  return finalStr;
+};
+
+console.log("\n");
+console.log("Question 1:");
+console.log(reverseString("hello"));
+
+// Question 2
+const reverseZigZagString = (string) => {
+  let finalOutput = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    if (i % 2 !== 0) {
+      finalOutput += string[i].toUpperCase();
+    } else {
+      finalOutput += string[i].toLowerCase();
+    }
+  }
+  return finalOutput;
+};
+
+console.log("\n");
+console.log("Question 2:");
+console.log(reverseZigZagString("hello"));
